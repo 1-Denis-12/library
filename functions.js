98d4b7acb1f5933fd5cd4bed416310b250e5eda8
Denis <Denispudrik@yahoo.com>
2024-11-23 21:10:21 -0500

    README.md

--- functions.js
+++ functions.js
@@ -9,13 +9,18 @@ function Book(title, author, year) {
 }
 
 const book1 = new Book("How to", "James May","2024")
-MyLibrary.push(book1)
+const book2 = new Book("hello", "my man","2022")
+MyLibrary.push(book1, book2)
 
 
 function addBookToLibrary(title, author, year) {
     const newBook = new Book(title, author, year);
-    MyLibrary.push(newBook0);
+    MyLibrary.push(newBook);
+    books.innerHTML = " "
+    displayBooks()
 }
+
+
 
 function displayBooks() {
     for (i = 0; i < MyLibrary.length; i++) {
@@ -23,8 +28,28 @@ function displayBooks() {
                             <h1>Title: ${MyLibrary[i].title}</h1>
                             <p>Author: ${MyLibrary[i].author}</p>
                             <p>Year: ${MyLibrary[i].year}</p>
+                            <button class="remove_btn" onclick="removeBook(${i})">remove</button>
                             </div>`
     }
 }
 
+function removeBook(num) {
+    MyLibrary.splice(num, 1)
+    books.innerHTML = " "
+    displayBooks()
+}
+
+function addBook() {
+    books.innerHTML += `<dialog open>
+                            <form method="dialog"> 
+                            <lable for="title">Title</lable>
+                            <input type="text" id="title" name="title">
+                            <lable for="author">Author</lable>
+                            <input type="text" id="author" name="author">
+                            <lable for="year">Year</lable>
+                            <input type="text" id="year" name="year">
+                            <button onclick="addBookToLibrary(this.form.title.value,this.form.author.value,this.form.year.value)">Add</button>
+                            </form>
+                        </dialog>`
+}
 displayBooks()
